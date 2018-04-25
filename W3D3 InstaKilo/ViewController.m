52a61557402e41f0a78114c0a22a94f0d5fed348
Colin Russell <colinrussell101@gmail.com>
2018-04-25 19:14:23 -0400

    final commit for everything except stretch goals

--- W3D3 InstaKilo/ViewController.m
+++ W3D3 InstaKilo/ViewController.m
@@ -17,8 +17,8 @@
 @property (strong, nonatomic) NSArray *animalImagesArray;
 @property (strong, nonatomic) NSArray *carImagesArray;
 @property (strong, nonatomic) NSArray *zooLocationImagesArray;
-@property (strong, nonatomic) NSArray *unknownLocationImagesArray;
 @property (strong, nonatomic) NSArray *homeLocationArray;
+@property (strong, nonatomic) NSArray *americaLocationArray;
 
 @end
 
@@ -49,8 +49,10 @@
     self.animalImagesArray = @[[UIImage imageNamed:@"IMG_0217.jpg"], [UIImage imageNamed:@"IMG_0251.jpg"], [UIImage imageNamed:@"IMG_0255.jpg"], [UIImage imageNamed:@"IMG_0262.jpg"], [UIImage imageNamed:@"IMG_0292.jpg"]];
     self.carImagesArray = @[[UIImage imageNamed:@"592f4169b74af41b008b5977-750-563.jpg"], [UIImage imageNamed:@"images.jpeg"], [UIImage imageNamed:@"LamborghiniRoadsterTA.jpg"]];
     
-    //locations
-    
+    //locations: zoo, unknown, home, and restaurant
+    self.zooLocationImagesArray = self.animalImagesArray;
+    self.homeLocationArray = @[[UIImage imageNamed:@"IMG_0478.JPG"], [UIImage imageNamed:@"IMG_1724.JPG"]];
+    self.americaLocationArray = @[[UIImage imageNamed:@"images.jpeg"], [UIImage imageNamed:@"LamborghiniRoadsterTA.jpg"]];
 }
 
 
@@ -66,26 +68,51 @@
 }
 
 - (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    if (section == 0 || section == 2) {
-        return 3;
+    
+    if (self.orderByLocation == NO){
+        if (section == 0 || section == 2) {
+            return 3;
+        } else {
+            return 5;
+        }
     } else {
-        return 5;
+        if (section == 0 || section == 1){// america or home
+            return 2;
+        } else if (section == 2 || section == 4){ // restaurant or unknown
+            return 1;
+        } else {
+            return 5; // zoo
+        }
+        
     }
 }
 
 - (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
     PhotoCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"PhotoCell" forIndexPath:indexPath];
-
+    
     cell.photoView.contentMode = UIViewContentModeScaleAspectFit;
     
-    if (indexPath.section == 0){
-        cell.photoView.image = [self.foodImagesArray objectAtIndex:indexPath.item];
-    } else if (indexPath.section == 1){
-        cell.photoView.image = [self.animalImagesArray objectAtIndex:indexPath.item];
-    } else {
-        cell.photoView.image = [self.carImagesArray objectAtIndex:indexPath.item];
+    if (self.orderByLocation == NO){
+        if (indexPath.section == 0){
+            cell.photoView.image = [self.foodImagesArray objectAtIndex:indexPath.item];
+        } else if (indexPath.section == 1){
+            cell.photoView.image = [self.animalImagesArray objectAtIndex:indexPath.item];
+        } else {
+            cell.photoView.image = [self.carImagesArray objectAtIndex:indexPath.item];
+        }
+    }else {
+        if (indexPath.section == 0){
+            cell.photoView.image = [self.americaLocationArray objectAtIndex:indexPath.item];
+        } else if (indexPath.section == 1){
+            cell.photoView.image = [self.homeLocationArray objectAtIndex:indexPath.item];
+        } else if (indexPath.section == 2){
+            cell.photoView.image = [UIImage imageNamed:@"IMG_0124.JPG"];
+        } else if (indexPath.section == 3){
+            cell.photoView.image = [self.zooLocationImagesArray objectAtIndex:indexPath.item];
+        } else {
+            cell.photoView.image = [UIImage imageNamed:@"592f4169b74af41b008b5977-750-563.jpg"];
+        }
     }
-    
     return cell;
 }
 
@@ -97,13 +124,23 @@
         if (self.orderByLocation == NO) {
             if (indexPath.section == 0){
                 label.text = @"Food";
-            } else if (indexPath.section == 1){
+            }else if (indexPath.section == 1){
                 label.text = @"Animals";
-            } else {
+            }else {
                 label.text = @"Cars";
             }
         } else {
-            
+            if (indexPath.section == 0) {
+                label.text = @"America";
+            }else if (indexPath.section == 1){
+                label.text = @"Home";
+            }else if (indexPath.section == 2) {
+                label.text = @"Restaurant";
+            }else if (indexPath.section == 3) {
+                label.text = @"Zoo";
+            }else {
+                label.text = @"Unknown Location";
+            }
         }
         return header;
     }
@@ -113,7 +150,12 @@
 #pragma mark - Actions
 
 - (IBAction)segmentedControl:(UISegmentedControl *)sender {
-    
+    if (self.orderByLocation == NO) {
+        self.orderByLocation = YES;
+    } else {
+        self.orderByLocation = NO;
+    }
+    [self.collectionView reloadData];
 }
 
 
